687ee6a9c2390e93b7172a99fe7fd30dc06c61af
jmilson- <julianamilson@hotmail.com>
2022-02-15 21:02:53 +0100

    function deleted

--- src/sort_utils.c
+++ src/sort_utils.c
@@ -12,24 +12,6 @@
 
 #include "push_swap.h"
 
-int	ft_sqrt(int nb)
-{
-	int	n;
-
-	n = 1;
-	if (nb <= 0)
-		return (0);
-	while (n <= 46340)
-	{
-		if (n * n == nb)
-			return (n);
-		else if (n * n > nb)
-			return (n - 1);
-		n++;
-	}
-	return (0);
-}
-
 int	rotate_stack_a(int count_rbs, t_stacks *stacks)
 {
 	if (count_rbs > 0)
